ea087f4edb8d5c5330ece6a557a518b0d8e3c9f7
Kevin Eleven <hiem.lhr@gmail.com>
2022-08-07 23:49:42 +0500

    Fixed title screen, level select and level1 scenes being to small
    title screen is now the scene that runs at the start

---
+++ Scripts/LevelSelect.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_Button_pressed_lvl1() -> void:
+	get_tree().change_scene("res://Scenes/Level1.tscn")
--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,16 +1,5 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
-func _on_Start_pressed():
+func _on_Start_pressed() -> void:
 	get_tree().change_scene("res://Scenes/LevelSelect.tscn")
